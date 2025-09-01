00206e061f8e179fe8f77cb41d9edb3d36783ba3
Rashidyounus <123Rashidmahmood@gmail.com>
2025-09-01 11:08:05 +0500

    sp file update

--- SP.sql
+++ SP.sql
@@ -110,17 +110,89 @@ EXECUTE uspFindProducts
     @name = 'Trek';
 
 
-	DECLARE @model_year SMALLINT;
-	DECLARE @model_year AS SMALLINT;
-			SET @model_year = 2018;
+DECLARE @model_year AS SMALLINT;
+SET @model_year = 2018;
 
 
 select * from production.products
 where model_year=2018
 
-DECLARE @model_year SMALLINT, 
-        @product_name VARCHAR(MAX);
+Declare @product_count int
+set @product_count = (select count (*) from production.products);
+--select @product_count
+print @product_count
+
+
+alter procedure useGetProductList (@model_year int)
+as 
+begin
+Declare @product_list varchar(max)
+set @product_list =''
+select @product_list = @product_list+product_name + char(10)
+from production.products
+where model_year=@model_year
+print (@product_list)
+End;
+
+
+execute useGetProductList 2018
 
 
 
+select @@ROWCOUNT
 
+Begin 
+    select product_name  from
+	production.products where list_price >100
+
+	if @@ROWCOUNT =0 
+	    print 'No product found';
+END;
+
+
+
+--- Nested Begin End
+
+
+BEGIN
+    DECLARE @name VARCHAR(MAX);
+
+    SELECT TOP 1
+        @name = product_name
+    FROM
+        production.products
+    ORDER BY
+        list_price DESC;
+    
+    IF @@ROWCOUNT <> 0
+    BEGIN
+        PRINT 'The most expensive product is ' + @name
+    END
+    ELSE
+    BEGIN
+        PRINT 'No product found';
+    END;
+END
+
+
+--- IF ELSE
+
+-- While
+-- print only 5
+Declare @count int
+set @count =0
+while @count <5
+    set @count =@count +1
+    print @count
+
+
+	---1,2,3,4,5
+Declare @count int
+set @count =0
+
+while @count <5
+begin
+
+    set @count =@count +1
+    print @count
+End
